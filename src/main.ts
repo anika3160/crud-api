ddb27147a2b9db7f860c4bcddc8236d2e6ea7c8e
Anna Surahatava <anika3160@gmail.com>
2023-01-11 17:15:08 +0100

    refactor: migrate users to up level

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import * as dotenv from 'dotenv';
-import server from './modules/server.js';
+import createServer from './modules/server.js';
+import process from 'node:process';
 
 dotenv.config();
-const PORT = process.env.PORT || 3000;
+const PORT:number = Number(process.env.PORT) || 3000;
+interface IUser {
+  readonly id: string,
+  username: string,
+  age: number,
+  hobbies: string[],
+}
+let users: IUser[] = [];
 
-server.listen(PORT, () => {
+const server = createServer(users);
+server.listen((PORT), () => {
     console.log(`Server is running on port ${PORT}`)
+})
+
+server.on('error', err => {
+    console.log(err)
 })
